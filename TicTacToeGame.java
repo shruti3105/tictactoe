6ca40f50c3c48bc9cb392833b2ab62e75bda118e
Shruti Verma <shrutiverma3105@gmail.com>
2021-04-25 21:26:37 +0530

    commit UC3

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -6,7 +6,8 @@ public static void main(String[] args) {
 	Scanner userInput=new Scanner(System.in);
 	char userLetter=chooseUserLetter(userInput);
 	char computerLetter = (userLetter =='X')?'0':'X'; 
-	}
+	showBoard(board);
+}
 
 private static char[] createBoard() {
 	char[] board= new char[10];
@@ -20,5 +21,16 @@ private static char[] createBoard() {
 private static char[] chooseLetter(Scanner userInput) {
 	System.out.println("Choose O or X: ");
 	return userInput.next().toUpperCase().charAt(0);
+}
+
+/* UC3 */
+public static void showBoard(char board[]) {
+	System.out.println("\n " + board[1] + " | " + board[2] + " | "+board[3]);
+	System.out.println("---------------------");
+	System.out.println("\n " + board[4] + " | " + board[5] + " | "+board[3]);
+	System.out.println("---------------------");
+	System.out.println("\n " + board[7] + " | " + board[8] + " | "+board[3]);
+	System.out.println("---------------------");
+
 }
 }
